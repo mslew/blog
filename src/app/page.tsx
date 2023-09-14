f5af243e7075a377256ce79f4c2d5664d8082a75
mslew <mlew3223@gmail.com>
2023-09-14 12:46:17 -0500

    created logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
+import Logo from '@/components/Logo'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-purple-500">this is a test hehe</h1>
       <Link href="/blog">This goes to da blog</Link>
+      <Logo />
     </main>
   )
 }
---
+++ src/components/Logo.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faM, faCircle } from '@fortawesome/free-solid-svg-icons'
+
+const Logo = () => {
+  return (
+    <div className='flex justify-center items-center'>
+        <FontAwesomeIcon className='w-6 absolute z-10 text-purple-600' icon={faM} />
+        <FontAwesomeIcon className='bg-black w-14 absolute text-gray-900 ring-4 ring-purple-600 rounded-full' icon={faCircle} />
+      </div>
+  )
+}
+
+export default Logo
