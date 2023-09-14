67b9b94b39e982a874e1d573f80cd95326789716
mslew <mlew3223@gmail.com>
2023-09-13 22:51:48 -0500

    added files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        '-2xl': {max: '1536px'},
+        '-xl': {max: '1280px'},
+        '-lg': {max: '1024px'},
+        '-md': {max: '768px'},
+        '-sm': {max: '640px'},
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
