b84dcd5afd5aede602e023023051ad9008151a69
mslew <mlew3223@gmail.com>
2023-09-15 14:26:25 -0500

    switching themes :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import Link from 'next/link'
 import Logo from '../components/Logo'
 import ThemeSwitch from '../components/ThemeSwitch'
+import { ThemeProvider } from 'next-themes' 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-purple-500">this is a test hehe</h1>
-      <Link href="/blog">This goes to da blog</Link>
-      <Logo />
-      <ThemeSwitch />
-    </main>
+    <ThemeProvider attribute='class'>
+      <main className="flex min-h-screen flex-col items-center p-24">
+        <h1 className="text-purple-500">this is a test hehe</h1>
+        <Link href="/blog">This goes to da blog</Link>
+        <Logo />
+        <ThemeSwitch />
+      </main>
+    </ThemeProvider>
   )
 }
--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -3,26 +3,28 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faToggleOn, faToggleOff, faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+import { useTheme } from 'next-themes';
 
 const ThemeSwitch = () => {
-    const [dark, setDark] = useState(true)
+    const { systemTheme, theme, setTheme} = useTheme();
+    const currentTheme = theme === 'system' ? systemTheme : theme;
 
     return (
         <button className='mt-20 flex justify-center items-center whitespace-normal flex-wrap rounded-3xl'
         onClick={() => {
-            setDark(!dark)
-        }}>
-            {dark ?
+            theme == "dark"? setTheme('light'): setTheme("dark")}
+        }>
+            {theme === 'dark' ?
             //dark mode on
             <div className='flex justify-center items-center'>
-                <FontAwesomeIcon className='absolute text-black z-10 mr-8' icon={faMoon} size='2x'/>
-                <FontAwesomeIcon className='absolute' icon={faToggleOn} size='5x'/>
+                <FontAwesomeIcon className='absolute text-black z-10 mr-5' icon={faMoon} size='sm'/>
+                <FontAwesomeIcon className='absolute' icon={faToggleOn} size='3x'/>
             </div>
             :
             //dark mode off
             <div className='flex justify-center items-center'>
-                <FontAwesomeIcon className='absolute text-white z-10 ml-8' icon={faSun} size='xl'/>
-                <FontAwesomeIcon className='absolute' icon={faToggleOff} size='5x'/>
+                <FontAwesomeIcon className='absolute text-white z-10 ml-5' icon={faSun} size='sm'/>
+                <FontAwesomeIcon className='absolute' icon={faToggleOff} size='3x'/>
             </div>
             }
         </button>
