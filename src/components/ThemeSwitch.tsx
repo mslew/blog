1c41d8e98efa5c8f542258ef6d7639aa1d2bceab
mslew <mlew3223@gmail.com>
2023-09-15 14:47:50 -0500

    slight change to ordering

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -8,26 +8,28 @@ import { useTheme } from 'next-themes';
 const ThemeSwitch = () => {
     const { systemTheme, theme, setTheme} = useTheme();
     const currentTheme = theme === 'system' ? systemTheme : theme;
+    console.log(theme)
 
-    return (
+    return (    
+        <>
         <button className='mt-20 flex justify-center items-center whitespace-normal flex-wrap rounded-3xl'
-        onClick={() => {
-            theme == "dark"? setTheme('light'): setTheme("dark")}
-        }>
-            {theme === 'dark' ?
+        onClick={() => {theme === "dark" ? setTheme('light') : setTheme("dark")}}>
+        
+            {theme === 'light' ?
             //dark mode on
             <div className='flex justify-center items-center'>
-                <FontAwesomeIcon className='absolute text-black z-10 mr-5' icon={faMoon} size='sm'/>
-                <FontAwesomeIcon className='absolute' icon={faToggleOn} size='3x'/>
+                <FontAwesomeIcon className='absolute text-black z-10 ml-5' icon={faSun} size='sm'/>
+                <FontAwesomeIcon className='absolute' icon={faToggleOff} size='3x'/>
             </div>
             :
             //dark mode off
             <div className='flex justify-center items-center'>
-                <FontAwesomeIcon className='absolute text-white z-10 ml-5' icon={faSun} size='sm'/>
-                <FontAwesomeIcon className='absolute' icon={faToggleOff} size='3x'/>
+                <FontAwesomeIcon className='absolute text-black z-10 mr-5' icon={faMoon} size='sm'/>
+                <FontAwesomeIcon className='absolute' icon={faToggleOn} size='3x'/>
             </div>
             }
         </button>
+        </>
     )
 }
 
