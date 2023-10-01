e9a82bddc2d4e455bddcb40b4502a93458bac5c5
mslew <mlew3223@gmail.com>
2023-10-01 00:23:38 -0500

    sveltekit

--- src/app/blog/[post]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Pog = () => {
-  return (
-    <div>This is a test</div>
-  )
-}
-
-export default Pog
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Home() {
-  return (
-      <main className="flex min-h-screen flex-col items-center p-24">
-        <h1 className="text-purple-500">this is a test hehe</h1>
-      </main>
-  )
-}
