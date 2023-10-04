6384c4b75f90b08dc2f17c7e2938eac9e865162c
mslew <mlew3223@gmail.com>
2023-10-04 00:12:02 -0500

    better layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,11 +4,13 @@ module.exports = {
 	theme: {
 		extend: {
 			gridTemplateRows: {
-				'layout': '8% repeat(7, minmax(0, 1fr))'
+				'8': 'repeat(8, minmax(0, 1fr))',
+				'10': 'repeat(10, minmax(0, 1fr))'
 			},
 			gridRow: {
 				'span-8': 'span 8 / span 8',
-				'span-7': 'span 7 / span 7'
+				'span-7': 'span 7 / span 7',
+				'span-10': 'span 10 / span 10'
 			}
 		},
 	},
