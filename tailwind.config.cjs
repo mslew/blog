80ab2338dc16517bc49ab2203c06c7aee6d4ae61
mslew <mlew3223@gmail.com>
2023-10-03 23:14:36 -0500

    update some more things

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateRows: {
+				'layout': '8% repeat(7, minmax(0, 1fr))'
+			},
+			gridRow: {
+				'span-8': 'span 8 / span 8',
+				'span-7': 'span 7 / span 7'
+			}
+		},
 	},
 	plugins: [],
 }
